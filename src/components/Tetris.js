89e6ad157c70001e747892b6a50efd50aa28a314
the-jey <jey.pley@gmail.com>
2020-03-07 15:17:01 +0100

    create the player rotation

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,7 +19,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
   
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
@@ -72,6 +72,9 @@ const Tetris = () => {
       // Down arrow on the keyboard
       } else if(keyCode === 40) {
         dropPlayer();
+      // Up array on the keyboard
+      } else if(keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   }
