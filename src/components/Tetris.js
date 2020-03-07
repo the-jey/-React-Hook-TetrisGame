95064b7e0c7c5a05243abd2d70d82921f5b111fd
the-jey <jey.pley@gmail.com>
2020-03-07 15:39:26 +0100

    create the drop with the useInterval

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -35,6 +36,7 @@ const Tetris = () => {
   const startGame = () => {
     // Reset everything
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
   }
@@ -55,8 +57,18 @@ const Tetris = () => {
     
   }
 
+  // Keyup function
+  const keyUp = ({ keyCode })  => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000);
+      }
+    }
+  }
+
   // DropPlayer function
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   }
 
@@ -79,11 +91,15 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime)
+
 
 
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
